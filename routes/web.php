bc1e8b83fb641c493dbc27fc5c24f489a112dc30
Patrick Her <renfu.her@gmail.com>
2024-04-08 10:34:54 +0800

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Backend\IndexController;
 
-Route::get('/', function () {
+
+Route::grt('/', function(){
     return view('welcome');
+});
+
+Route::group(['prefix' => 'backend.'], function(){
+    Route::get('/', [IndexController::class, 'index'])->name('backend.home');
 });
