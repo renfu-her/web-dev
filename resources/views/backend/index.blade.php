d377c01baa5af81c8863a62fa48c3b8ea4e64fc7
Patrick Her <renfu.her@gmail.com>
2024-04-08 11:46:23 +0800

    update

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid px-4">
             <h1 class="mt-4">Tables</h1>
             <ol class="breadcrumb mb-4">
-                <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
                 <li class="breadcrumb-item active">Tables</li>
             </ol>
             <div class="card mb-4">
@@ -14,7 +14,7 @@
                     DataTable Example
                 </div>
                 <div class="card-body">
-                    <table id="datatablesSimple">
+                    <table id="dataTable">
                         <thead>
                             <tr>
                                 <th>Name</th>
@@ -505,8 +505,8 @@
 
 @section('js')
     <script>
-        
-        let table = new DataTable('#datatablesSimple');
+
+        let table = new DataTable('#dataTable');
 
     </script>
 @endsection
