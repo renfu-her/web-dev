37401387db7e99849baffa67189d603202dc8f24
Patrick Her <renfu.her@gmail.com>
2024-04-08 11:20:30 +0800

    update

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -10,6 +10,8 @@
     <title>Admin 管理系統</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="{{ asset('admin/css/styles.css') }}" rel="stylesheet" />
+    <link href="{{ asset('admin/css/custom.css?t=' . time()) }}" rel="stylesheet" />
+
     @yield('css')
 
 </head>
@@ -145,8 +147,6 @@
 
     
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
