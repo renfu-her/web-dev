6becf3dd6ff5f92579bf3016c0f7f24c1e672f97
Patrick Her <renfu.her@gmail.com>
2024-04-08 12:17:57 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $faker = Faker::create();
+        $gender = $faker->randomElement(['male', 'female']);
+        foreach (range(1, 200) as $index) {
+            DB::table('students')->insert([
+                'name' => $faker->name($gender),
+                'email' => $faker->email,
+                'username' => $faker->username,
+                'phone' => $faker->phoneNumber,
+                'dob' => $faker->date($format = 'Y-m-d', $max = 'now')
+            ]);
+        }
     }
 }
