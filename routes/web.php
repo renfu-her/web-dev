2ab4495857f2c56891c02fbbd8544a5d2e485310
Patrick Her <renfu.her@gmail.com>
2024-04-08 12:52:03 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\StudentController;
+
 use App\Http\Controllers\Backend\IndexController;
 
 
 Route::get('/', function(){
-    return view('welcome');
+    return 'OK';
 });
+
+Route::get('students', [StudentController::class, 'index']);
+Route::get('students/list', [StudentController::class, 'getStudents'])->name('students.list');
 
 Route::group(['prefix' => 'backend', 'name' => 'backend.'], function(){
     Route::get('/', [IndexController::class, 'index'])->name('backend.home');
