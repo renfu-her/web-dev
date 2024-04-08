70f0d349b9225f347845e7d95ed71630853ca9da
Patrick Her <renfu.her@gmail.com>
2024-04-08 11:23:25 +0800

    update

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -506,5 +506,6 @@
 @section('js')
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
         crossorigin="anonymous"></script>
+        
     <script src="{{ asset('admin/js/datatables-simple-demo.js') }}"></script>
 @endsection
