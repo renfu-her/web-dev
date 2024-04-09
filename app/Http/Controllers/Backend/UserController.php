10d61fb14d4fda397464398c506ab33672cdfee5
Patrick Her <renfu.her@gmail.com>
2024-04-09 21:09:35 +0800

    update

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -17,9 +17,24 @@ class UserController extends Controller
 
     public function show(Request $request, $id)
     {
+        if ($request->ajax()) {
+            $data = User::with('group')->latest()->get();
 
-        $this->getUsers($request);
-
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->addColumn('user_group', function ($row) {
+                    // 檢查是否存在 user_group 關聯並顯示相應的名稱，否則顯示 "N/A"
+                    return $row->group ? $row->group->name : 'N/A';
+                })
+                ->addColumn('action', function ($row) {
+                    $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm"><i class="fa-solid fa-pen-to-square"></i> 編輯</a> 
+                                  <a href="javascript:void(0)" class="delete btn btn-danger btn-sm"><i class="fa-solid fa-trash-can"></i> 刪除</a>';
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+     
     }
 
     public function store()
