7296a1410a0d3e6e5ebe31d56880a51dcd942fe2
Patrick Her <renfu.her@gmail.com>
2024-04-09 09:06:34 +0800

    update index

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -13,7 +13,7 @@
                     <i class="fas fa-table me-1"></i>
                     使用者管理
                 </div>
-                <div class="card-body table-responsive">
+                <div class="card-body table-responsive" style="width: 100%">
                     <table class="table table-bordered" id="dataTable">
                         <thead>
                             <tr>
